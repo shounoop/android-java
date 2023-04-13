c82a3f7f58414a944cf1dd9f80feb79f8add02f6
Shounoop <hmtuan9a@gmail.com>
2023-04-14 01:50:15 +0700

    Update UI of SQLite lesson

---
+++ lessions/SQLite/app/src/main/java/com/example/sqlite/adapter/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.sqlite.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.sqlite.fragment.FragmentHistory;
+import com.example.sqlite.fragment.FragmentHome;
+import com.example.sqlite.fragment.FragmentSearch;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 1:
+                return new FragmentHistory();
+            case 2:
+                return new FragmentSearch();
+            default:
+                return new FragmentHome();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
